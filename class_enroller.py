0ebeb9ec08a607e6acfbba6da4c8194db746793d
Lily Gostovic <lily.gostovic@gmail.com>
2022-01-13 09:33:54 -0500

    Add function to input crn

--- class_enroller.py
+++ class_enroller.py
@@ -14,9 +14,9 @@ driver.get("https://horizon.mcgill.ca/pban1/twbkwbis.P_WWWLogin")
 
 
 # vars from user
-un=''
-pw=''
-crn=''
+un='lilith.gostovic@mail.mcgill.ca'
+pw='C1r1mel167'
+crn='12345'
 
 def login():
     WebDriverWait(driver, 10).until(EXP_COND.presence_of_element_located((By.ID, 'mcg_un')))
@@ -38,8 +38,16 @@ def navigate_to_registration():
         WebDriverWait(driver, 10).until(EXP_COND.presence_of_element_located((By.XPATH, xpath)))
         driver.find_element_by_xpath(xpath).click()
 
+def input_crn():
+    WebDriverWait(driver, 10).until(EXP_COND.presence_of_element_located((By.ID, 'crn_id1')))
+    driver.find_element_by_id('crn_id1').send_keys(crn)
+
+    WebDriverWait(driver, 10).until(EXP_COND.presence_of_element_located((By.XPATH, "//div[@class='pagebodydiv']//form//input[19]")))
+    WebDriverWait(driver, 10).until(EXP_COND.element_to_be_clickable((By.XPATH, "//div[@class='pagebodydiv']//form//input[19]")))
+    driver.find_element_by_xpath("//div[@class='pagebodydiv']//form//input[19]").click()
   
 login()
 navigate_to_registration()
+input_crn()
 
 # driver.quit()
