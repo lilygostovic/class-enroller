e3a9c775815923e250a4c4065434e4d9aaa2f46f
Lily Gostovic <lily.gostovic@gmail.com>
2022-01-13 08:29:09 -0500

    Login finished.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 A tool to register for McGill classes during add/drop period
+
+requirements:
+- python
+- selenium (pip3 install selenium)
+- webdriver manager
+- install chrome driver for your computer and put in folder
--- class_enroller.py
+++ class_enroller.py
@@ -1,3 +1,30 @@
 #!/usr/bin/env python
 
-print("hello world")
+# imports
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EXP_COND
+
+# setup chrome driver
+driver=webdriver.Chrome('./chromedriver')
+driver.maximize_window()
+driver.get("https://horizon.mcgill.ca/pban1/twbkwbis.P_WWWLogin")
+
+
+
+# vars from user
+un=''
+pw=''
+crn=''
+
+def login():
+    WebDriverWait(driver, 20).until(EXP_COND.presence_of_element_located((By.ID, 'mcg_un')))
+    WebDriverWait(driver, 20).until(EXP_COND.presence_of_element_located((By.ID, 'mcg_pw')))
+    driver.find_element_by_id('mcg_un').send_keys(un)
+    driver.find_element_by_id('mcg_pw').send_keys(pw)
+    driver.find_element_by_id('mcg_un_submit').click()
+  
+login()
+
+driver.quit()
